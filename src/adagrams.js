5de3c00da02d2516af1f63a17c28c9fb4dc23fb2
Olga-allgood <olga.s.orlova@gmail.com>
2025-11-20 14:25:06 -0800

    wave2

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,9 +1,23 @@
 const handSize = 10;
+const bonusMinLength = 7;
+const lengthBonusPoints = 8;
+const pairWord = 0;
+const pairScore = 1;
 
 const letterPool = {
   A: 9, B: 2, C: 2, D: 4, E: 12, F: 2, G: 3, H: 2, I: 9, J: 1,
   K: 1, L: 4, M: 2, N: 6, O: 8, P: 2, Q: 1, R: 6, S: 4, T: 6,
   U: 4, V: 2, W: 2, X: 1, Y: 2, Z: 1
+};
+
+const scoreChart = {
+  A: 1, E: 1, I: 1, O: 1, U: 1, L: 1, N: 1, R: 1, S: 1, T: 1,
+  D: 2, G: 2,
+  B: 3, C: 3, M: 3, P: 3,
+  F: 4, H: 4, V: 4, W: 4, Y: 4,
+  K: 5,
+  J: 8, X: 8,
+  Q: 10, Z: 10
 };
 
 export const drawLetters = () => {
@@ -33,8 +47,30 @@ export const drawLetters = () => {
   return handOfLetters;
 };
 
-export const usesAvailableLetters = (input, lettersInHand) => {
-  // Implement this method for wave 2
+const createLetterFrequencyDict = (letters) => {
+  const letterFrequencyDict = {};
+  letters.forEach(letter => {
+    letter = letter.toUpperCase();
+    letterFrequencyDict[letter] = (letterFrequencyDict[letter] || 0) + 1;
+  });
+  return letterFrequencyDict;
+};
+
+// Wave 2: usesAvailableLetters
+export const usesAvailableLetters = (word, letterBank) => {
+  const upperWord = word.toUpperCase();
+  const upperLetterBank = letterBank.map(letter => letter.toUpperCase());
+
+  const wordDict = createLetterFrequencyDict(upperWord.split(''));
+  const letterBankDict = createLetterFrequencyDict(upperLetterBank);
+
+  for (const letter in wordDict) {
+    if (!(letter in letterBankDict) || wordDict[letter] > letterBankDict[letter]) {
+      return false;
+    }
+  }
+
+  return true;
 };
 
 export const scoreWord = (word) => {
